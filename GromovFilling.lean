084afc1dd3d75764143af7f80418baef5269c041
chenle02 <chenle02@gmail.com>
2026-08-29 16:33:57 -0500

    Formalize nonlinear symplectic calculus

--- GromovFilling.lean
+++ GromovFilling.lean
@@ -38,6 +38,7 @@ import GromovFilling.MetricPlanarCoverage
 import GromovFilling.ModTwoDegree
 import GromovFilling.NormalFormBoundaryObstruction
 import GromovFilling.Numerics
+import GromovFilling.NonlinearSymplecticCalculus
 import GromovFilling.OneHighLeg
 import GromovFilling.Oriented
 import GromovFilling.PlanarCertificate
